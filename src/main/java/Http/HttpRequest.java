d5dc88426abe67877090910eb565f8ff8123ba07
niubilityjin <niubilityjin@gmail.com>
2018-02-03 00:06:08 +0800

    第三版更新,之前不太会用,新的说明在readme里

--- src/main/java/Http/HttpRequest.java
+++ src/main/java/Http/HttpRequest.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 
 public class HttpRequest {
 	/**
-	 * mm
 	 * 用于读取客户端发送过来数据的输入流,该流应该是通过Socket获取的输入流.
 	 */
 	private InputStream in;
@@ -13,18 +12,38 @@ public class HttpRequest {
 	 * 请求行相关信息
 	 */
 	private String method;
+	
 	//请求资源路径
 	private String url;
+	
 	//请求使用的HTTP协议版本
 	private String protocl;
 	
-	public HttpRequest(InputStream in) {
-		System.out.println("HttpRequest构造方法执行");
+	/**
+	 * 实例化HttpRequest
+	 * @param in
+	 * 
+	 */
+	public HttpRequest(InputStream in){
+		System.out.println("HttpRequest:构造方法执行");
 		this.in=in;
+		/*
+		 * 每个请求包含三部分解析:
+		 * 1:解析请求行
+		 * 2:解析消息头
+		 * 3:解析消息正文
+		 */
 		parseRequestLine();
-		System.out.println("HttpRequest构造方法执行完毕");
+		
+		//打桩
+		System.out.println("HttpRequest:构造方法执行完毕");
 	}
-	private void parseRequestLine() {
+	/**
+	 * 解析请求行
+	 * @param in
+	 */
+	private void parseRequestLine(){
+		//打桩
 		System.out.println("开始解析请求行");
 		//读取请求行内容
 		String requestLine=readLine();
@@ -35,35 +54,47 @@ public class HttpRequest {
 		method=array[0];
 		
 		url=array[1];
-		
+		System.out.println("url:"+url);
 		protocl=array[2];
 		
+
+		//打桩
 		System.out.println("请求行:"+requestLine);
+		
 	}
 	/**
 	 * 读取一行字符串(以CRLF作为结尾),返回的字符串中不包含最后的CRLF
 	 * @return
 	 */
-	private String readLine() {
-		StringBuilder builder=new StringBuilder();
+	private String readLine(){
+		StringBuilder builder= new StringBuilder();
 		try {
 			int d=-1;
-			char c1='a';
-			char c2='a';
-			while((d=in.read())!=-1) {
+			char c1= 'a';//上一次读到的值
+			char c2= 'a';//本次读到的值
+			while((d=in.read())!=-1){
 				c2=(char)d;
-				if(c1==13&&c2==10) {
+				//CR编码是13  LF编码是10
+				if(c1==13&&c2==10){
 					break;
 				}
 				builder.append(c2);
 				c1=c2;
 			}
-			return builder.toString().trim();
+			return builder.toString().trim();	
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		}
 		return "";
+	}
+	public String getMethod() {
+		return method;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public String getProtocl() {
+		return protocl;
 	}
 
 }
--- src/main/java/webserver/ClientHandler.java
+++ src/main/java/webserver/ClientHandler.java
@@ -1,8 +1,14 @@
 package webserver;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+
+import Http.HttpRequest;
 
 /**
  * 
@@ -17,14 +23,68 @@ public class ClientHandler implements Runnable{
 	}
 	public void run(){
 		try {
-			/*
-			 * 测试读取客户端发送过来的Http请求内容
-			 */
-			InputStream in=socket.getInputStream();
-			int d=-1;
-			while((d=in.read())!=-1){
-				System.out.print((char)d);
+			//打桩
+			System.out.println("ClientHandler:开始处理请求");
+			
+			//解析请求信息
+			HttpRequest request= new HttpRequest(socket.getInputStream());
+			//打桩
+			System.out.println("CilentHandler:url:"+request.getUrl());
+			File file=new File("webapps"+request.getUrl());
+			System.out.println("文件路径:"+"webapps"+request.getUrl());
+			//打桩
+			System.out.println("文件是否存在:"+file.exists());
+			if(file.exists()){
+				//将该文件发送回给客户端
+				OutputStream out= socket.getOutputStream();
+				/*
+				 * 回复HTTP响应
+				 */
+				//1 发送状态行
+				String line="HTTP/1.1 200 OK";
+				out.write(line.getBytes("ISO8859-1"));
+				out.write(13);
+				out.write(10);
+				//打桩
+				System.out.println("状态行发送完毕");
+				
+				//2 发送响应头
+				line="Content-Type:text/html";
+				out.write(line.getBytes("ISO8859-1"));
+				out.write(13);
+				out.write(10);
+				
+				//打桩
+				System.out.println("响应头发送完毕");
+				
+				//3 发送响应正文
+				
+				line="Content-Length:"+file.length();
+				out.write(line.getBytes("ISO8859-1"));
+				out.write(13);
+				out.write(10);
+				//单独发送CRLF表示响应头部分发送完毕
+				out.write(13);
+				out.write(10);
+				//打桩
+				System.out.println("响应头部分发送完毕.");
+				
+				
+				//发送响应正文
+				FileInputStream fis=new FileInputStream(file);
+				BufferedInputStream bis=new BufferedInputStream(fis);
+				byte [] buf=new byte[1024*10];
+				int len=-1;
+				while((len=bis.read(buf))!=-1){
+					out.write(buf,0,len);
+				}
+				bis.close();
+				//打桩
+				System.out.println("正文响应完毕");
 			}
+
+
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally{
